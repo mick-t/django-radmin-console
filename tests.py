be1ee2bfd42e1169ef10eb0b04283fa242dcdf48
Angel Medrano <asmedrano@gmail.com>
2012-10-25 22:29:42 -0400

    cleanup'

--- tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from radmin.utils import *
-from radmin.console import *
-
-class TestUtils(TestCase):
-    def test_radmin_import(self):
-        """ testing loading a module by string """
-        impo = radmin_import('radmin.views.sample')
-        self.assertEqual(impo(), 'Hi there!')
--- urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('radmin.views',
-    url(r'^ep/$', 'entry_point'),
-    url(r'^rnr/$', 'runner'),
-)
