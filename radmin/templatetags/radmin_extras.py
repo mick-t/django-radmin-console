d851aae653ce87aed9b9f6ac3cf7f5312672a08c
Piotr Radkowski <piotr.radkowski@ardigen.com>
2016-07-22 14:50:17 +0200

    Check if original object is None

--- radmin/templatetags/radmin_extras.py
+++ radmin/templatetags/radmin_extras.py
@@ -38,8 +38,9 @@ def get_admin_context(context):
     try:
         # editing a model
         ctx['location']= 'model_edit'
-        ctx['param1'] = context['original'].__class__.__name__.lower()
-        ctx['param2'] = context['original'].pk
+        if context['original'] is not None:
+            ctx['param1'] = context['original'].__class__.__name__.lower()
+            ctx['param2'] = context['original'].pk
         return json.dumps(ctx)
     except KeyError:
         pass
